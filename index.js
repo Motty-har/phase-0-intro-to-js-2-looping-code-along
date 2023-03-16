397983ec7452cf947f9d04b82fb1f4005d17e126
Motty-har <harligmotty@gmail.com>
2023-03-15 20:10:35 -0700

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    const letter = []
+    for (let i = 0; i < name.length; i++){
+        letter.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return letter
+}
+
+function countDown(num){
+    let number = num
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
